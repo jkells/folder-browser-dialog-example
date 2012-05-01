c1ae898dedae6e9e8d35f9378bcb76434d7ee0b8
Jared Kells <jkells@gmail.com>
2012-05-02 09:48:12 +1000

    FolderBrowserEx Example

---
+++ Form1.Designer.cs
@@ -0,0 +1,61 @@
+﻿namespace FolderBrowserExample
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.openButton = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // openButton
+            // 
+            this.openButton.Location = new System.Drawing.Point(12, 12);
+            this.openButton.Name = "openButton";
+            this.openButton.Size = new System.Drawing.Size(136, 23);
+            this.openButton.TabIndex = 0;
+            this.openButton.Text = "Open File Or Folder";
+            this.openButton.UseVisualStyleBackColor = true;
+            this.openButton.Click += new System.EventHandler(this.openButtonClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(292, 273);
+            this.Controls.Add(this.openButton);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button openButton;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+
+namespace FolderBrowserExample
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void openButtonClick(object sender, EventArgs e)
+        {
+            var dlg1 = new Ionic.Utils.FolderBrowserDialogEx();
+            dlg1.Description = "Select a file or folder";
+            dlg1.ShowNewFolderButton = true;
+            dlg1.ShowEditBox = true;
+            dlg1.ShowBothFilesAndFolders = true;
+            dlg1.ShowFullPathInEditBox = true;
+            dlg1.RootFolder = System.Environment.SpecialFolder.MyComputer;
+
+            // Show the FolderBrowserDialog.
+            DialogResult result = dlg1.ShowDialog();
+            if (result == DialogResult.OK)
+            {
+                var path = dlg1.SelectedPath;
+                if(Directory.Exists(dlg1.SelectedPath))
+                {
+                    MessageBox.Show("Directory selected: " + dlg1.SelectedPath);
+                }
+                else{
+                    MessageBox.Show("File selected: " + dlg1.SelectedPath);
+                }
+            }
+        }
+    }
+}
